82397baacde461df4968aea4b1f082574c35b85a
david medina <rafaelsanchez97.dm@gmail.com>
2023-04-20 11:05:48 -0300

    prueba admin6 static files agregados

--- backend/mercadito/settings.py
+++ backend/mercadito/settings.py
@@ -17,7 +17,8 @@ from dotenv import load_dotenv
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
