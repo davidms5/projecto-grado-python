184bd2ea706643cf5b29893e05a70ddc04dabdee
David Medina <107662095+davidms5@users.noreply.github.com>
2023-06-27 00:57:55 -0300

    Update settings.py

--- backend/mercadito/settings.py
+++ backend/mercadito/settings.py
@@ -36,13 +36,13 @@ CSRF_TRUSTED_ORIGINS = ["https://projecto-grado-python-production.up.railway.app
 "https://projecto-grado-python-frontend-production.up.railway.app",]
 # Application definition
 
-CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
-    "http://localhost:8000",
-    "https://projecto-grado-python-production.up.railway.app",
-    "https://projecto-grado-python-frontend.vercel.app",
-    "projecto-grado-python-frontend-production.up.railway.app",
-]
+#CORS_ORIGIN_WHITELIST = [
+#    "http://localhost:3000",
+#    "http://localhost:8000",
+#    "https://projecto-grado-python-production.up.railway.app",
+#    "https://projecto-grado-python-frontend.vercel.app",
+#    "projecto-grado-python-frontend-production.up.railway.app",
+#]
 CORS_ORIGIN_ALLOW_ALL=True
 CORS_ALLOW_CREDENTIALS = True
 CSRF_COOKIE_SECURE = False
